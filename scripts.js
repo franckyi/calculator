d3feb15bb55194d61f54cdb829deb05e9768db90
francky <francky@tuta.io>
2023-09-15 08:29:25 +0200

    minor updates

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const backBtn = document.querySelector('.backspace');
 let result = '';
 
 function showHearts() {
-    numOnBottom.innerHTML = '💕 Love U 💕'
+    numOnBottom.innerHTML = '❤️ Love U ❤️'
 }
 
 function addOperands() {
